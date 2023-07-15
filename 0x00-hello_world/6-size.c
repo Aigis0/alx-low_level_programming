a8b5ff8b2ceeebea5534b5ef128e07cfa5d6cf89
Muhammad Bashir Umar <muhdbashir261@gmail.com>
2023-07-14 19:03:56 -0700

    Preprocessor C programing

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,9 @@
  */
 int main(void)
 {
-	printf("Size of a char: 1 byte(s)");
-	printf("Size of an int: 4 byte(s)");
-	printf("Size of a long int: 4 byte(s)");
-	printf("Size of a long long int: 8 byte(s)");
-	printf("Size of a float: 4 byte(s)");
-     return (0);
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
+	printf("Size of int: %lu byte(s)\n", sizeof(int));
+ 	printf("Size of long int: %lu byte(s)\n", sizeof(long));
+    	printf("Size of long long int: %lu byte(s)\n", sizeof(long long));
+   	printf("Size of float: %lu byte(s)\n", sizeof(float));
 }
