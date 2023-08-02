32c7cc840af811349a2eeb8ecf4f207b5078243f
Muhammad Bashir Umar <muhdbashir261@gmail.com>
2023-08-02 16:46:33 -0700

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void){
+	if (*s){
+		_purtchar(*s);
+		_put_recursion(s + 1);
+	else
+		printf('/n');
+		return 0;
+}
