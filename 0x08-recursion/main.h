b93bef5453bd5c1d3005e65a9c6fc732675b8027
Muhammad Bashir Umar <muhdbashir261@gmail.com>
2023-08-02 17:47:52 -0700

    recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 void _puts_recursion(char *s);
-<<<<<<< HEAD
 int _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -8,6 +10,5 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
+
 #endif
-=======
->>>>>>> 32c7cc840af811349a2eeb8ecf4f207b5078243f
