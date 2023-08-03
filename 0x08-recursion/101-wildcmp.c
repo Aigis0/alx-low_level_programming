6b07991667a0641a0767a020ba152477514d4ace
Muhammad Bashir Umar <muhdbashir261@gmail.com>
2023-08-02 18:01:30 -0700

    recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h" 
 
 int wildcmp(char *s1, char *s2) {
   if (*s2 == '\0') {
@@ -8,34 +8,4 @@ int wildcmp(char *s1, char *s2) {
   } else {
     return (*s1 == *s2) && wildcmp(s1 + 1, s2 + 1);
   }
-}
-
-int main() {
-  int r;
-
-  r = wildcmp("main.c", "*.c");
-  printf("%d\n", r);
-  r = wildcmp("main.c", "m*a*i*n*.*c*");
-  printf("%d\n", r);
-  r = wildcmp("main.c", "main.c");
-  printf("%d\n", r);
-  r = wildcmp("main.c", "m*c");
-  printf("%d\n", r);
-  r = wildcmp("main.c", "ma********************************c");
-  printf("%d\n", r);
-  r = wildcmp("main.c", "*");
-  printf("%d\n", r);
-  r = wildcmp("main.c", "***");
-  printf("%d\n", r);
-  r = wildcmp("main.c", "m.*c");
-  printf("%d\n", r);
-  r = wildcmp("main.c", "**.*c");
-  printf("%d\n", r);
-  r = wildcmp("main-main.c", "ma*in.c");
-  printf("%d\n", r);
-  r = wildcmp("main", "main*d");
-  printf("%d\n", r);
-  r = wildcmp("abc", "*b");
-  printf("%d\n", r);
-  return (0);
 }
