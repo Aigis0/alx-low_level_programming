47b5c3442878392bbadb3a1e4edcda5fde86e15f
Muhammad Bashir Umar <muhdbashir261@gmail.com>
2023-11-03 21:25:27 -0700

    string concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,20 +10,20 @@
  * Return: point to memory of concatenated string
  */
 
-char *string_nconcat(char *s1, char *s2, unsigned int n);
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i;
-	int s1len = 0;
-	int s2len = 0;
+	unsigned int i;
+	unsigned int s1len = 0;
+	unsigned int s2len = 0;
 	char *output;
 
-	if (s1len == NULL)
+	if (s1 == NULL)
 		s1 = "";
-	if (s2len == NULL)
+	if (s2 == NULL)
 		s2 = "";
-	for (i = 0; s1len[i] != '/0'; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 	       s1len++;
-	for (i = 0; s2len[i] != '/0'; i++)
+	for (i = 0; s2[i] != '\0'; i++)
 		s2len++;
 
 	output = malloc(sizeof(char) * (s1len * n) + 1);
@@ -31,17 +31,18 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 		return (NULL);
 	if (n >= s2len)
 	{
-		for (i = 0; s1[i] != '/0'; i++)
+		for (i = 0; s1[i] != '\0'; i++)
 			output[i] = s1[i];
-		for (i = 0; s2[i] != '/0'; i++)
+		for (i = 0; s2[i] != '\0'; i++)
 			output[s1len + i] = s2[i];
-		output[s1len + i] = '/0';
+		output[s1len + i] = '\0';
 	}
 	else
-		for (i = 0; s1[i] != '/0'; i++)
+	{
+		for (i = 0; s1[i] != '\0'; i++)
 			output[i] = s1[i];
 		for (i = 0; i < n; i++)
 			output[s1len + i] = s2[i];
-		output[s1len + i] = '/0';
+		output[s1len + i] = '\0';
 	}
-	return (output);
+	return (output);}
