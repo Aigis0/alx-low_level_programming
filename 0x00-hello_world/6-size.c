53021c5b0e4e5f5b94f64803db43cb363d02e327
Muhammad Bashir Umar <muhdbashir261@gmail.com>
2023-12-17 17:40:57 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,14 @@
-#!/include <stdio.h>
-
+#include <stdio.h>
 /**
  * main - entry point
  * printf: prints statement
  * Return: 0 if true
  */
 int main(void)
-(
+{
 	printf("Size of a char: %lu byte(s)", sizeof(char));
 	printf("Size of an int: %lu byte(s)", sizeof(int));
 	printf("Size of a long int: %lu byte(s)", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)", sizeof(float));
+}
